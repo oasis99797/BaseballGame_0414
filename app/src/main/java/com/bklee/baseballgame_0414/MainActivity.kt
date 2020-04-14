ce264fbda96afbb35095c88c1b87d6562d99fcfe
oasis99797 <oasis99797@gmail.com>
2020-04-14 22:22:03 +0900

    입력검사 로직 추가

--- app/src/main/java/com/bklee/baseballgame_0414/MainActivity.kt
+++ app/src/main/java/com/bklee/baseballgame_0414/MainActivity.kt
@@ -2,11 +2,16 @@ package com.bklee.baseballgame_0414
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.bklee.baseballgame_0414.adapters.ChatAdapter
 import com.bklee.baseballgame_0414.datas.Chat
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+
+    //    문제 숫자 세자리가 담길 ArrayList
+    val computerNumbers = ArrayList<Int>()
 
     val chatings = ArrayList<Chat>()
     var mChatAdapter:ChatAdapter? = null
@@ -16,10 +21,62 @@ class MainActivity : BaseActivity() {
         setContentView(R.layout.activity_main)
         setupEvents()
         setValues()
+        makeComputerNumber()
     }
 
+    //    컴퓨터가 문제 출제하는 함수
+    fun makeComputerNumber() {
+
+        for (i in 0..2) {
+
+            while (true) {
+
+//            1. 1~9의 숫자만 사용 => 랜덤으로 생성되어야함.
+//            2. 이미 뽑힌 숫자는 사용하면 안됨. (중복허용 X)
+//      1 <= Math.random()*9+1 < 10
+                val tempNum = (Math.random()*9+1).toInt()
+
+//        중복검사 => 컴퓨터 숫자 배열안에 이 임시번호가 같은게 있다면?
+                var isDuplOk = true
+
+                for (cpNum in computerNumbers) {
+                    if (tempNum == cpNum) {
+                        isDuplOk = false
+                    }
+                }
+
+                if (isDuplOk) {
+                    computerNumbers.add(tempNum)
+                    break
+                }
+            }
+
+        }
+
+        for (num in computerNumbers) {
+            Log.d("출제번호", num.toString())
+        }
+
+
+    }
 
     override fun setupEvents() {
+
+        okBtn.setOnClickListener {
+
+            if (inputEdt.text.toString().length != 3) {
+                Toast.makeText(mContext, "세자리 숫자로 입력해주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (inputEdt.text.toString().contains(" ")) {
+                Toast.makeText(mContext, "숫자만 입력해주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            chatings.add(Chat(inputEdt.text.toString(), "USER"))
+            mChatAdapter?.notifyDataSetChanged()
+        }
 
     }
 
--- app/src/main/java/com/bklee/baseballgame_0414/adapters/ChatAdapter.kt
+++ app/src/main/java/com/bklee/baseballgame_0414/adapters/ChatAdapter.kt
@@ -5,10 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import android.widget.TextView
 import com.bklee.baseballgame_0414.R
 import com.bklee.baseballgame_0414.datas.Chat
 
-class ChatAdapter(context: Context, resId:Int, list:ArrayList<Chat>) : ArrayAdapter<Chat>(context, resId, list) {
+class ChatAdapter(context:Context, resId:Int, list:ArrayList<Chat>) : ArrayAdapter<Chat>(context, resId, list) {
 
     val mContext = context
     val mList = list
@@ -22,6 +24,26 @@ class ChatAdapter(context: Context, resId:Int, list:ArrayList<Chat>) : ArrayAdap
             tempRow = inf.inflate(R.layout.chat_list_item, null)
         }
         val row = tempRow!!
+
+        val computerLayout = row.findViewById<LinearLayout>(R.id.computerLayout)
+        val userLayout = row.findViewById<LinearLayout>(R.id.userLayout)
+        val computerTxt = row.findViewById<TextView>(R.id.computerTxt)
+        val userTxt = row.findViewById<TextView>(R.id.userTxt)
+
+        val data = mList.get(position)
+
+        if (data.speaker == "COMPUTER") {
+            computerLayout.visibility = View.VISIBLE
+            userLayout.visibility = View.GONE
+
+            computerTxt.text = data.content
+        }
+        else {
+            computerLayout.visibility = View.GONE
+            userLayout.visibility = View.VISIBLE
+
+            userTxt.text = data.content
+        }
 
 
         return row
