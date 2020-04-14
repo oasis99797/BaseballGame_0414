31a0b944fc8eaa26ff1577c62b28d0a75fa5594b
oasis99797 <oasis99797@gmail.com>
2020-04-14 22:07:15 +0900

    어댑터 / 리스트뷰 연결

--- app/src/main/java/com/bklee/baseballgame_0414/BaseActivity.kt
+++ app/src/main/java/com/bklee/baseballgame_0414/BaseActivity.kt
@@ -1,6 +1,10 @@
 package com.bklee.baseballgame_0414
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import com.bklee.baseballgame_0414.adapters.ChatAdapter
+import com.bklee.baseballgame_0414.datas.Chat
 
 abstract class BaseActivity : AppCompatActivity() {
 
